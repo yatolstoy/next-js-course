593b507c1287df7c850761ca3b6be87222d455d3
Tolstoy Yaroslav <rastorop92@yandex.ru>
2022-04-14 10:46:35 +0300

    add: components HTAG and Button

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   reactStrictMode: true,
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,12 @@ class MyDocument extends Document {
 	render(): JSX.Element {
 		return (
 			<Html lang='ru'>
-				<Head/>
+				<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+				</Head>
+
 				<body>
 						<Main/>
 						<NextScript/>
