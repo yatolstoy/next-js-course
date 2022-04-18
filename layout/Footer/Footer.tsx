f59eea50bbe70ea8be79e1e17f445d41a3743965
Tolstoy Yaroslav <rastorop92@yandex.ru>
2022-04-18 12:14:13 +0300

    add: footer layout

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,14 @@
 import { FooterProps } from './Footer.props'
 import cn from 'classnames';
-import styles from './Header.module.css';
+import styles from './Footer.module.css';
+import {format} from 'date-fns';
 
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
 	return (
-		<div {...props}>
-			Footer
-		</div>
+		<footer className={cn(styles.footer, className)} {...props}>
+			<p>OwlTop &copy; 2020 - {format(new Date(), 'yyyy')} Все права защищены</p>
+			<a href='#'>Пользовательское соглашение</a>
+			<a href='#'>Политика конфиденциальности</a>
+		</footer>
 	)
 }
