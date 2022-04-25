6787d57b6080bb0139c3d9a04fc0e856aa3e8819
Tolstoy Yaroslav <rastorop92@yandex.ru>
2022-04-25 15:19:51 +0300

    add: animation on hide and show review form

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -17,6 +17,17 @@ const ProductComponent = forwardRef(({product, className, ...props}: ProductProp
 	const [isReviewOpened, setIsReviewOpened] = useState<boolean>(false)
 	const reviewRef = useRef<HTMLDivElement>(null);
 
+	const variants = {
+		visible: {
+			opacity: 1,
+			height: 'auto'
+		},
+		hidden: {
+			opacity: 0,
+			height: 0
+		}
+	}
+
 	const scrollToReview = () => {
 		setIsReviewOpened(true);
 		reviewRef.current?.scrollIntoView({
@@ -96,20 +107,19 @@ const ProductComponent = forwardRef(({product, className, ...props}: ProductProp
 			</div>
 
 		</Card>
-		<Card color={'blue'} className={cn(styles.reviews, {
-			[styles.opened]: isReviewOpened,
-			[styles.closed]: !isReviewOpened
-		})} ref={reviewRef}>
-			{
-				product.reviews.map(review => (
-					<div key={review._id}>
-						<Review review={review} />
-						<Divider />
-					</div>
-				))
-			}
-			<ReviewForm productId={product._id}/>
-		</Card>
+		<motion.div animate={isReviewOpened ? 'visible' : 'hidden'} variants={variants} initial='hidden'>
+			<Card color={'blue'} className={cn(styles.reviews)} ref={reviewRef}>
+				{
+					product.reviews.map(review => (
+						<div key={review._id}>
+							<Review review={review} />
+							<Divider className={styles.hr}/>
+						</div>
+					))
+				}
+				<ReviewForm productId={product._id}/>
+			</Card>
+		</motion.div>
 		</div>
 	)
 })
