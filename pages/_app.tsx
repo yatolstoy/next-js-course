2c351532021cbba45a3d13301ae9cf680a6a53ec
Tolstoy Yaroslav <rastorop92@yandex.ru>
2022-04-27 18:53:15 +0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import React from 'react'
 import Head from 'next/head'
 
+
 function MyApp({ Component, pageProps, router }: AppProps) {
   return <>
     <Head>
